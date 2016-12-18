4e1c15486fa44720a3ecc6db33110bf08c798be7
dhineshwaranr <dhinesh.kr13@gmail.com>
2016-12-19 04:39:21 +0530

    First Initial Commit

---
+++ pala/src/main/java/com/techaffinity/pala/PalaApplication.java
@@ -0,0 +1,12 @@
+package com.techaffinity.pala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalaApplication.class, args);
+	}
+}
